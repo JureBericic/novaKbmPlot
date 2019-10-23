c691719b0c9b430d2f3071e4cab3d0e3d944e55e
Jure Bericic <jure.bericic@gmail.com>
2019-10-23 23:34:18 +0200

    Defined transformer.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const parse = require('csv-parse');
+const transform = require('stream-transform');
 const fs = require('fs');
 
 const data = [];
@@ -6,6 +7,16 @@ const data = [];
 const parser = parse({
     delimiter: ';',
     from_line: 2
+});
+
+const transformer = transform(function(line, cb) {
+    const transformed = {
+        date: new Date(line[4].split('.').reverse().join('-')),
+        amount: parseAmount(line[5]) - parseAmount(line[6]),
+        currency: line[7]
+    };
+
+    cb(null, transformed);
 });
 
 function parseAmount(amount) {
@@ -22,10 +33,12 @@ fs
     // parse csv
     .pipe(parser)
     // transform each line into needed form
-    .on('data', (line) => data.push({
-        date: new Date(line[4].split('.').reverse().join('-')),
-        amount: parseAmount(line[5]) - parseAmount(line[6]),
-        currency: line[7],
-    }))
+    // .on('data', (line) => data.push({
+    //     date: new Date(line[4].split('.').reverse().join('-')),
+    //     amount: parseAmount(line[5]) - parseAmount(line[6]),
+    //     currency: line[7],
+    // }))
+    .pipe(transformer)
+    .on('data', (line) => data.push(line))
     // print data at the end
     .on('end', () => {console.table(data)});
