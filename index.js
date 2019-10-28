e20a6c12f63ca9361bd8162ec1b9f31f1c15c84a
Jure Bericic <jure.bericic@gmail.com>
2019-10-28 22:51:17 +0100

    Doing this synchronously instead of with streams.

--- index.js
+++ index.js
@@ -1,44 +1,78 @@
-const parse = require('csv-parse');
-const transform = require('stream-transform');
+const parse = require('csv-parse/lib/sync');
 const fs = require('fs');
 
-const data = [];
-
-const parser = parse({
-    delimiter: ';',
-    from_line: 2
-});
-
-const transformer = transform(function(line, cb) {
-    const transformed = {
-        date: new Date(line[4].split('.').reverse().join('-')),
-        amount: parseAmount(line[5]) - parseAmount(line[6]),
-        currency: line[7]
-    };
-
-    cb(null, transformed);
-});
 
 function parseAmount(amount) {
-    if (amount === "") {
-        return 0;
-    }
-
-    return parseFloat(amount.replace(',', '.'));
+    return amount !== "" ? parseFloat(amount.replace('.', '').replace(',', '.')) : 0;
 }
 
-fs
-    // read file
-    .createReadStream('./data/promet_test.csv')
-    // parse csv
-    .pipe(parser)
-    // transform each line into needed form
-    // .on('data', (line) => data.push({
-    //     date: new Date(line[4].split('.').reverse().join('-')),
-    //     amount: parseAmount(line[5]) - parseAmount(line[6]),
-    //     currency: line[7],
-    // }))
-    .pipe(transformer)
-    .on('data', (line) => data.push(line))
-    // print data at the end
-    .on('end', () => {console.table(data)});
+function round(value, decimals) {
+    return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+}
+
+
+const records = parse(  // parse csv file
+    fs.readFileSync(
+        './data/promet.csv',
+        { encoding: 'utf-8' }
+    ),
+    {
+        delimiter: ';',
+        from_line: 2,
+        relax_column_count: true  // lines without 'NAMEN' filled, are one column too short...
+    }
+).map(r => ({  // map columns into useful data
+    date: new Date(r[4].split('.').reverse().join('-')),
+    amount: parseAmount(r[5]) - parseAmount(r[6]),
+    currency: r[7]
+}))
+.reverse()  // last transactions are first in the file, want it around
+.reduce(  // aggregate data by months
+    (acc, curr) => {
+        let currentStatistics = acc[acc.length - 1];
+        if (!currentStatistics) {
+            // create first entry
+            currentStatistics = {
+                year: curr.date.getFullYear(),
+                month: curr.date.getMonth(),
+                start: 0,
+                end: 0,
+                min: 0,
+                max: 0,
+                total: 0
+            };
+            acc.push(currentStatistics);
+        } else if (currentStatistics.month !== curr.date.getMonth() || currentStatistics.year !== curr.date.getFullYear()) {
+            // create new entry
+            currentStatistics = {
+                year: curr.date.getFullYear(),
+                month: curr.date.getMonth(),
+                end: 0,
+                start: currentStatistics.end,
+                end: currentStatistics.end,
+                min: currentStatistics.end,
+                max: currentStatistics.end,
+                total: 0
+            };
+            acc.push(currentStatistics);
+        }
+        // update entry
+        currentStatistics.end += curr.amount;
+        currentStatistics.min = currentStatistics.end < currentStatistics.min ? currentStatistics.end : currentStatistics.min;
+        currentStatistics.max = currentStatistics.end > currentStatistics.max ? currentStatistics.end : currentStatistics.max;
+        currentStatistics.total += curr.amount;
+
+        return acc;
+    },
+    []
+).map(s => {  // round all numbers because of floating point arithmetics
+    s.start = round(s.start, 2);
+    s.end = round(s.end, 2);
+    s.min = round(s.min, 2);
+    s.max = round(s.max, 2);
+    s.total = round(s.total, 2);
+    
+    return s;
+});
+
+console.table(records);
